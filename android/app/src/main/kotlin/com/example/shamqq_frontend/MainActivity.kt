aefba5ccf32513956f7498f111e3526a0e7fdb9a
nipengg <nevillecorneliustj@gmail.com>
2021-12-02 02:03:25 +0700

    Create project, Import Image and Font Assets

---
+++ android/app/src/main/kotlin/com/example/shamqq_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shamqq_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+        Text("Hello World", style: GoogleFonts.poppins(fontSize: 50),),
+      ),
+    );
+  }
+}
