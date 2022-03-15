b4d8b1fc36f9d8162c430e5c65e0eaacaaa16571
nipengg <nevillecorneliustj@gmail.com>
2022-03-16 01:31:41 +0700

    create message model and service, setup db in firebase

---
+++ lib/services/message_service.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shamqq_frontend/models/product_model.dart';
+import 'package:shamqq_frontend/models/user_model.dart';
+
+class MessageService{
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<void> addMessage({UserModel user, bool isFromUser, String message, ProductModel product}) async {
+    try {
+      firestore.collection('messages').add({
+        'userId': user.id,
+        'userName': user.name,
+        'userImage': user.profilePhotoUrl,
+        'isFromUser': true,
+        'messages': message,
+        'product': product is UninitializedProductModel ? {} : product.toJson(),
+        'createdAt': DateTime.now().toString(),
+        'updatedAt': DateTime.now().toString(),
+      }).then((value) => print('Sent Message Success!'));
+    } catch (e) {
+      throw Exception('Sent Message Failed');
+    }
+  }
+
+}
